a53d56c9bd7e28309ef71fa25c73e7337c5212a7
Artem <artem.chemolosov@outlook.com>
2022-09-07 16:21:45 -0500

    init commit

---
+++ encoder.py
@@ -0,0 +1,13 @@
+#transforming input symbols to ASCII values
+def cchrm():
+    print(meenc[i], ord(meenc[i]))
+def ordn():
+    ord(meenc[i])
+meenc = input(('Enter a message to encode: '))
+for i in range(len(meenc)):
+    cchrm()
+#changing the ASCII value to an alternate number
+
+
+
+    
